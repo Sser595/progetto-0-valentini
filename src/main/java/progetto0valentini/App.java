1991de44dcba79b6a2b69d393541b5be203310b6
Serena Valentini <valentini.serena@itismeucci.com>
2023-09-27 08:29:40 +0200

    primo commit

---
+++ src/main/java/progetto0valentini/App.java
@@ -0,0 +1,13 @@
+package progetto0valentini;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
